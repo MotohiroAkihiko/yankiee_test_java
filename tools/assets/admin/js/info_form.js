286da90bed7ceae1037605bef5de7ab98496ba7d
Akihiko Motohiro <a.motohiro@4qualia.co.jp>
2018-07-04 11:58:36 +0900

    info削除ボタン追加

--- tools/assets/admin/js/info_form.js
+++ tools/assets/admin/js/info_form.js
@@ -4,12 +4,19 @@
 $(function() {
 
 	$(document).ready(function() {
-		
 		$('#button-delete').on('click', function(e) {
 			if (confirm('削除します。よろしいですか？')) {
 				$('#form-delete').submit();
 			}
 		});
-		
+
+		$('.index-button').on('click', function(e) {
+			if (confirm('削除します。よろしいですか？')) {
+				$('#form-delete').submit();
+			} else {
+				return false;
+			}
+		});
 	});
 });
+
