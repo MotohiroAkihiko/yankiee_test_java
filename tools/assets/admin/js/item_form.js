2ffefa6ac8b5f5eb60968fdf69f555e2bed42562
Akihiko Motohiro <a.motohiro@4qualia.co.jp>
2018-07-04 11:59:59 +0900

    item.jsを追加

---
+++ tools/assets/admin/js/item_form.js
@@ -0,0 +1,24 @@
+/*
+ * アイテム画面JS
+ */
+$(function() {
+
+	$(document).ready(function() {
+		$('#button-delete').on('click', function(e) {
+			if (confirm('削除します。よろしいですか？')) {
+				$('#form-delete').submit();
+			}
+		});
+
+		$('.index-button').on('click', function(e) {
+			if (confirm('削除します。よろしいですか？')) {
+				$('#form-delete').submit();
+			} else {
+				return false;
+			}
+		});
+
+
+	});
+
+});
