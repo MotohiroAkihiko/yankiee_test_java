0a4fa011220b2fc2e45f04459b4113fad0f84b42
Akihiko Motohiro <a.motohiro@4qualia.co.jp>
2018-07-25 13:17:51 +0900

    課題②修正

--- tools/assets/admin/js/json.js
+++ tools/assets/admin/js/json.js
@@ -1,6 +1,7 @@
 $(function(){
 
 	var setArea = $('#loadarea'),
+	defaultNum = 5,
     loadNum = 5, // 読み込む個数
     loadTxt = 'Now Loading...', // Loading中の表示テキスト
     btnTxt = '追加読込', // ボタンテキスト
@@ -9,12 +10,107 @@ $(function(){
     setArea.after('<div id="btnMore" class="action button-blue middle">' + btnTxt + '</div>');
     var setMore = setArea.next('#btnMore');
 
+    $.ajax({
+        url: './json/json_viwe',
+        dataType: 'json',
+        success : function(data) {
+
+          var dataLength = data.length,
+          loadItemLength = setArea.find('.loadItem').length;
+
+          if(!(dataLength == loadItemLength)){
+            if(loadItemLength == 0){
+              if(dataLength <= defaultNum){
+                for (var i=0; i<dataLength; i++) {
+                	var tbody = document.getElementById('list');
+
+					if(data[i]['publish_end_date'] == null){
+						data[i]['publish_end_date'] = "";
+					}
+
+					if(data[i]['item_category_id'] == "1"){
+						data[i]['item_category_id'] = "1:食べ物";
+					} else if(data[i]['item_category_id'] == "2"){
+						data[i]['item_category_id'] = "2:季節もの";
+					} else {
+						data[i]['item_category_id'] = "3:ヤンキー";
+					}
+
+					//trエレメントを新規作成
+					var tr = document.createElement('tr');
+						tr.className = 'loadItem';
+
+					tr.innerHTML ='<td>'
+						+ data[i]['id'] + '</td><td>'
+						+ data[i]['publish_start_date'] + '</td><td>'
+						+ data[i]['publish_end_date'] + '</td><td>'
+						+ data[i]['item_category_id'] + '</td><td>'
+						+ data[i]['item_name'] + '</td><td>'
+						+ data[i]['item_details'] + '</td><td>'
+						+ data[i]['item_expire_seconds'] + '秒</td><td>'
+						+ data[i]['item_point_up_rate'] + '%</td><td>'
+						+ '<img src="/assets/admin/img/photo/' + data[i]['photo_saved_as'] + '" width="85" height="85"></td><td>'
+						+ data[i]['upd_date'] + '</td>';
+
+					//trエレメントをtbody内に追加
+					tbody.appendChild(tr);
+                }
+                setMore.hide();
+              } else {
+                for (var i=0; i<defaultNum; i++) {
+                	var tbody = document.getElementById('list');
+
+					if(data[i]['publish_end_date'] == null){
+						data[i]['publish_end_date'] = "";
+					}
+
+					if(data[i]['item_category_id'] == "1"){
+						data[i]['item_category_id'] = "1:食べ物";
+					} else if(data[i]['item_category_id'] == "2"){
+						data[i]['item_category_id'] = "2:季節もの";
+					} else {
+						data[i]['item_category_id'] = "3:ヤンキー";
+					}
+
+					//trエレメントを新規作成
+					var tr = document.createElement('tr');
+						tr.className = 'loadItem';
+
+					tr.innerHTML ='<td>'
+						+ data[i]['id'] + '</td><td>'
+						+ data[i]['publish_start_date'] + '</td><td>'
+						+ data[i]['publish_end_date'] + '</td><td>'
+						+ data[i]['item_category_id'] + '</td><td>'
+						+ data[i]['item_name'] + '</td><td>'
+						+ data[i]['item_details'] + '</td><td>'
+						+ data[i]['item_expire_seconds'] + '秒</td><td>'
+						+ data[i]['item_point_up_rate'] + '%</td><td>'
+						+ '<img src="/assets/admin/img/photo/' + data[i]['photo_saved_as'] + '" width="85" height="85"></td><td>'
+						+ data[i]['upd_date'] + '</td>';
+
+					//trエレメントをtbody内に追加
+					tbody.appendChild(tr);
+                }
+                setMore.fadeIn();
+              }
+            }
+          }else{
+            $('<p>アイテムが登録されてません。</p>').appendTo(setArea).css({opacity:'0'}).animate({opacity:'1'},fadeSpeed);
+            setMore.hide();
+          }
+        }, //success
+        complete : function(){
+          $('#nowLoading').each(function(){
+            $(this).remove();
+          });
+          return false;
+        } //complete
+      });
+
     setMore.click(function(){
 
 		// Ajax 通信の実行
 		$.ajax({
-
-			type: 		'POST',
 			url: 		'./json/json_viwe',
 			dataType: 	'json',
 
@@ -31,80 +127,7 @@ $(function(){
 
 				if(!(dataLengh == loadItemLength)){
 			        setArea.append('<div id="nowLoading">' + loadTxt + '</div>');
-			        if(loadItemLength == 0){
-			            if(dataLengh <= loadNum){
-			                for (var i=0; i<dataLengh; i++) {
-			                    var tbody = document.getElementById('list');
-
-								if(data[i]['publish_end_date'] == null){
-									data[i]['publish_end_date'] = "";
-								}
-
-								if(data[i]['item_category_id'] == "1"){
-									data[i]['item_category_id'] = "1:食べ物";
-								} else if(data[i]['item_category_id'] == "2"){
-									data[i]['item_category_id'] = "2:季節もの";
-								} else {
-									data[i]['item_category_id'] = "3:ヤンキー";
-								}
-
-								//trエレメントを新規作成
-								var tr = document.createElement('tr');
-									tr.className = 'loadItem';
-
-								tr.innerHTML ='<td>'
-									+ data[i]['id'] + '</td><td>'
-									+ data[i]['publish_start_date'] + '</td><td>'
-									+ data[i]['publish_end_date'] + '</td><td>'
-									+ data[i]['item_category_id'] + '</td><td>'
-									+ data[i]['item_name'] + '</td><td>'
-									+ data[i]['item_details'] + '</td><td>'
-									+ data[i]['item_expire_seconds'] + '秒</td><td>'
-									+ data[i]['item_point_up_rate'] + '%</td><td>'
-									+ '<img src="/assets/admin/img/photo/' + data[i]['photo_saved_as'] + '" width="85" height="85"></td><td>'
-									+ data[i]['upd_date'] + '</td>';
-
-								//trエレメントをtbody内に追加
-								tbody.appendChild(tr);
-			                }
-			                setMore.remove();
-			            } else {
-			                for (var i=0; i<loadNum; i++) {
-			                    var tbody = document.getElementById('list');
-
-								if(data[i]['publish_end_date'] == null){
-									data[i]['publish_end_date'] = "";
-								}
-
-								if(data[i]['item_category_id'] == "1"){
-									data[i]['item_category_id'] = "1:食べ物";
-								} else if(data[i]['item_category_id'] == "2"){
-									data[i]['item_category_id'] = "2:季節もの";
-								} else {
-									data[i]['item_category_id'] = "3:ヤンキー";
-								}
-
-								//trエレメントを新規作成
-								var tr = document.createElement('tr');
-									tr.className = 'loadItem';
-
-								tr.innerHTML ='<td>'
-									+ data[i]['id'] + '</td><td>'
-									+ data[i]['publish_start_date'] + '</td><td>'
-									+ data[i]['publish_end_date'] + '</td><td>'
-									+ data[i]['item_category_id'] + '</td><td>'
-									+ data[i]['item_name'] + '</td><td>'
-									+ data[i]['item_details'] + '</td><td>'
-									+ data[i]['item_expire_seconds'] + '秒</td><td>'
-									+ data[i]['item_point_up_rate'] + '%</td><td>'
-									+ '<img src="/assets/admin/img/photo/' + data[i]['photo_saved_as'] + '" width="85" height="85"></td><td>'
-									+ data[i]['upd_date'] + '</td>';
-
-								//trエレメントをtbody内に追加
-								tbody.appendChild(tr);
-			                }
-			            }
-			        } else if(loadItemLength > 0 && loadItemLength < dataLengh){
+			        if(loadItemLength > 0 && loadItemLength < dataLengh){
 			            if(loadNum < setAdj){
 			                for (var i=0; i<loadNum; i++) {
 			                    v = i+setBeg;
